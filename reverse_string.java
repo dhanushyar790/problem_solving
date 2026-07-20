80d0072df00d0b2d00d48d687374c58f1ca4be9d
Dhanushya <dhanushya790@gmail.com>
2026-07-20 23:25:34 +0530

    Create reverse_string.java

---
+++ reverse_string.java
@@ -0,0 +1,17 @@
+class Solution {
+    public int reverse(int x) {
+       int rev=0;
+       while(x!=0){
+        int lastdigit=x%10;
+        if(rev>Integer.MAX_VALUE/10 || rev==Integer.MAX_VALUE/10 && lastdigit>7){
+            return 0;
+        }
+        else if(rev<Integer.MIN_VALUE/10 || rev==Integer.MIN_VALUE/10 && lastdigit<-8){
+            return 0;
+        }
+        rev=rev*10+lastdigit;
+        x/=10;
+       }
+        return rev;
+    }
+}
