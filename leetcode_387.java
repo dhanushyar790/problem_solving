d6ead3583be650bb43e96bd875d7298dd0eef238
Dhanushya <dhanushya790@gmail.com>
2026-07-23 23:31:41 +0530

    Create leetcode_387.java

---
+++ leetcode_387.java
@@ -0,0 +1,19 @@
+class Solution {
+    public int firstUniqChar(String s) {
+        HashMap<Character,Integer> frequency=new HashMap<>();
+        for(int i=0;i<s.length();i++){
+            if(!frequency.containsKey(s.charAt(i))){
+                frequency.put(s.charAt(i),1);
+            }
+            else{
+                frequency.put(s.charAt(i),frequency.get(s.charAt(i))+1);
+            }
+        }
+        for(int i=0;i<s.length();i++){
+            if(frequency.get(s.charAt(i))==1){
+                return i;
+            }
+        }
+        return -1;
+    }
+}
